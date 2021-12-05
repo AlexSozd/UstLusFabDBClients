01aeed2e0f9397fc1f9bef65bc89716aa9d559d5
AlexSozd <55886669+AlexSozd@users.noreply.github.com>
2021-12-05 17:01:04 +0300

    Oracle database test content - revision

---
+++ oracle-db/Data_pocket1_2_2.sql
@@ -0,0 +1,13 @@
+INSERT INTO Persons VALUES (1, 'Сервисный центр LG Russia', ' ', 1, '127018, Москва, Комсомольский проспект, д.14/1. к.3',
+'7(499)372-49-99', 'corp@lg-russia.ru');
+INSERT INTO Persons VALUES (2, 'Центральный офис в России', ' ', 2, '197341, Санкт-Петербург, Коломяжский пр., д.26',
+ '8(499)404-18-94', 'info@hakko-shop.ru');
+ 
+SELECT * FROM Goods;
+SELECT * FROM Type_of_Good;
+SELECT * FROM Firms;
+SELECT * FROM Persons;
+SELECT * FROM Orders;
+SELECT * FROM OrderDetails;
+SELECT * FROM Supplies;
+SELECT * FROM SupplyDetails;
