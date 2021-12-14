5da07bfa9099c7248f45ddfc5eb47a7743846b8a
AlexSozd <55886669+AlexSozd@users.noreply.github.com>
2021-12-14 17:17:50 +0300

    Configuration setting for the backup operation

--- config1.py
+++ config1.py
@@ -1,13 +1,12 @@
 import sys,os
 config = {}
 
-#	Get connection object
+# Get connection object
 def GetConfig():
-	return config
+    return config
 
-#	Create connection string for defined user\password
-def MakeCS(user,password):
-	config_copy = GetConfig()
-	res = user + '/' + password + '@' + config_copy['SQLhostL'] + '/' + config_copy['SQLinstance']
-	return res
-
+# Create connection string for defined user\password
+def MakeCS(user, password):
+    config_copy = GetConfig()
+    res = user + '/' + password + '@' + config_copy['SQLhostL'] + '/' + config_copy['SQLinstance']
+    return res
